e7e65ccbfdeeb0fb2ee2b162cfec56f641714960
senso-viktor <30976717+senso-viktor@users.noreply.github.com>
2017-08-26 17:14:41 +0200

    prvy uspesny test hybania realnnou SCAROU (moveit aj C)

--- getSignalID.m
+++ getSignalID.m
@@ -1,4 +1,4 @@
-function [J1_pos_id, J2_pos_id, J3_pos_id, J1_torq_id, J2_torq_id, J3_mo_id, J3_mc_id, J3_pb_id, J3_lb_id] =getSignalID (tg)
+function [J1_pos_id, J2_pos_id, J3_pos_id, J1_torq_id, J2_torq_id, J3_mo_id, J3_mc_id, J3_pb_id] =getSignalID (tg)
 
     %positions
     J1_pos_id = tg.getsignalidsfromlabel('sigA1Unireg12Position');
@@ -13,6 +13,6 @@ function [J1_pos_id, J2_pos_id, J3_pos_id, J1_torq_id, J2_torq_id, J3_mo_id, J3_
     J3_mo_id = tg.getsignalidsfromlabel('sigA3magnetOpen');
     J3_mc_id = tg.getsignalidsfromlabel('sigA3magnetClose');
     J3_pb_id = tg.getsignalidsfromlabel('sigA3pushButton');
-    J3_lb_id = tg.getsignalidsfromlabel('sigA3lightBarrier');
+    %J3_lb_id = tg.getsignalidsfromlabel('sigA3lightBarrier');
 
 end
